90eb23a94ca728a8122bf969a169bd5c29f4083b
Mark Graham <mark.graham@kcl.ac.uk>
2020-04-20 14:47:35 +0100

    Pass arguments through command line and make variable names clearer

--- validate.py
+++ validate.py
@@ -1,50 +1,50 @@
 import csv
 import pipeline
 import dataset
+import argparse
 
 equivalence_map = {
     'na': ('', 'na'),
     '': ('', 'na')
 }
 
-def compare_row(eindex, expected, aindex, actual, keys):
+def compare_row(expected_index, expected_row, actual_index, actual_row, keys):
     all_true = True
     for k in keys:
-        evalue = expected.value_from_fieldname(eindex, k)
-        avalue = actual.value_from_fieldname(aindex, k)
-        if evalue in equivalence_map:
-            all_true = all_true and avalue in equivalence_map[evalue]
+        expected_value = expected_row.value_from_fieldname(expected_index, k)
+        actual_value = actual_row.value_from_fieldname(actual_index, k)
+        if expected_value in equivalence_map:
+            all_true = all_true and actual_value in equivalence_map[expected_value]
         else:
-            all_true = all_true and evalue == avalue
-
+            all_true = all_true and expected_value == actual_value
     return all_true
 
-def compare(expected, actual):
+def compare(expected, actual, verbose):
+    print(f'Comparing expected {expected} and current {actual}')
 
     with open(expected) as f:
         expected_ds = dataset.Dataset(f)
-    # with open(expected) as f:
-    #     expected_ds.parse_file(f)
     expected_field_names = expected_ds.names_
     with open(actual) as f:
         actual_ds = dataset.Dataset(f)
-    # with open(actual) as f:
-    #     actual_ds.parse_file(f)
     actual_field_names = actual_ds.names_
 
+    # compare field names
     if expected_field_names == actual_field_names:
         print('field names are identical')
     else:
-        print('only in expected:', set(expected_field_names).difference(set(actual_field_names)))
-        print('only in actual:', set(actual_field_names).difference(set(expected_field_names)))
-
-    # print(csvfpe.line_num)
+        print('WARNING: only in expected:', set(expected_field_names).difference(set(actual_field_names)))
+        print('WARNING: only in actual:', set(actual_field_names).difference(set(expected_field_names)))
 
     common_fields = set(expected_field_names).intersection(set(actual_field_names))
-    print('common fields:', common_fields)
+    if verbose:
+        print('common fields:', common_fields)
 
-    print(expected_ds.row_count())
-    print(actual_ds.row_count())
+    # compare data content
+    if expected_ds.row_count() == actual_ds.row_count():
+        print(f'both files have {expected_ds.row_count()} rows')
+    else:
+        print(f'WARNING: expected file has {expected_ds.row_count()} rows, actual file has {actual_ds.row_count()} rows')
 
     disparities = 0
     for i in range(expected_ds.row_count()):
@@ -52,14 +52,14 @@ def compare(expected, actual):
                 disparities += 1
 
     if disparities > 0:
-        print('rows not equal')
+        print('WARNING: rows not equal')
     else:
         print('rows equal')
 
 def validate(expected_patients_file_name, actual_patient_file_name,
-             expected_assessments_file_name, actual_assessments_file_name):
-    compare(expected_patients_file_name, actual_patient_file_name)
-    compare(expected_assessments_file_name, actual_assessments_file_name)
+             expected_assessments_file_name, actual_assessments_file_name, verbose=False):
+    compare(expected_patients_file_name, actual_patient_file_name, verbose)
+    compare(expected_assessments_file_name, actual_assessments_file_name, verbose)
 
 def validate_full():
     filename = '/home/ben/covid/assessments_20200413050002_clean.csv'
@@ -84,8 +84,19 @@ def show_rows(filename, fields_to_show):
             pipeline.print_diagnostic_row(f'{i_f}:', ds, ds.fields_, i_f, fields_to_show)
 
 
-epfn = 'v0.1.2_50k_patients.csv'
-apfn = 'test_patients.csv'
-eafn = 'v0.1.2_50k_assessments.csv'
-aafn = 'test_assessments.csv'
-validate(epfn, apfn, eafn, aafn)
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-pe', '--patients_expected',
+                        help='the location of the patient file produced by a correct version (currently v.0.1.2) of the pipeline')
+    parser.add_argument('-pa', '--patients_actual',
+                        help='the location of the patient file produced by the current version of the pipeline')
+    parser.add_argument('-ae', '--assessments_expected',
+                        help='the location of the assessments file produced by a correct version (currently v.0.1.2) of the pipeline')
+    parser.add_argument('-aa', '--assessments_actual',
+                        help='the location of the assessments file produced by the current version of the pipeline')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='increase verbosity')
+    args = parser.parse_args()
+    validate(args.patients_expected, args.patients_actual, args.assessments_expected, args.assessments_actual,
+             verbose=args.verbose)
