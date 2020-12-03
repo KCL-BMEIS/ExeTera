f852b5d8bc561d706e0fad016056f2567bf6a4f6
Ben Murray <ben.murray@gmail.com>
2020-12-03 09:35:26 +0000

    Updating setup.py to have readme based long description

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 from exetera import __version__
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='exetera',
     version=__version__,
     description='High-volume key-value store and analytics, based on hdf5',
-    long_description="file: README.md",
+    long_description=long_description,
     long_description_content_type = "text / markdown; charset = UTF - 8",
     url='https://github.com/kcl-bmeis/ExeTera',
     author='Ben Murray',
