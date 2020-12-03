182bbe335deb4936200ad353fd6202a14cc39b2e
Ben Murray <ben.murray@gmail.com>
2020-12-03 09:29:28 +0000

    Added missing long_description / content type fields to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     name='exetera',
     version=__version__,
     description='High-volume key-value store and analytics, based on hdf5',
+    long_description="file: README.md",
+    long_description_content_type = "text / markdown; charset = UTF - 8",
     url='https://github.com/kcl-bmeis/ExeTera',
     author='Ben Murray',
     author_email='benjamin.murray@kcl.ac.uk',
