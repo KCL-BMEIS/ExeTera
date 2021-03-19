6a118bb7cf105f39c699a19027a41dabb35423a4
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2021-03-19 00:25:07 +0000

    Doc changes
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -28,6 +28,7 @@ author = 'Benjamin Murray, KCL'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +37,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
---
+++ docs/exetera.rst
@@ -0,0 +1,20 @@
+exetera package
+===============
+
+Subpackages
+-----------
+
+.. toctree::
+   :maxdepth: 4
+
+   exetera.bin
+   exetera.core
+   exetera.processing
+
+Module contents
+---------------
+
+.. automodule:: exetera
+   :members:
+   :undoc-members:
+   :show-inheritance:
