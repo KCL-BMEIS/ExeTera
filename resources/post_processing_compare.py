b4d4f48ec28b8ae0ea295b1821c0b95a99b8a66c
Liyuan Chen <liyuan.chen.1024@gmail.com>
2021-06-08 10:02:47 +0100

    update post processing script

--- resources/post_processing_compare.py
+++ resources/post_processing_compare.py
@@ -2,13 +2,18 @@ import h5py
 
 old_source = 'old_vaccine_doses.hdf5'
 new_source = 'new_vaccine_doses.hdf5'
+schema_key = 'vacccine_doses'
+
+# old_source = 'old_vacc_hesitancy.hdf5'
+# new_source = 'new_vacc_hesitancy.hdf5'
+# schema_key = 'vaccine_hesitancy'
 
 # compare if they have same fields
 with h5py.File(old_source, 'r') as hf1:
-	old_fields_set = set(hf1['vaccine_doses'].keys())
+	old_fields_set = set(hf1[schema_key].keys())
 
 with h5py.File(new_source, 'r') as hf2:
-	new_fields_set = set(hf2['vaccine_doses'].keys())
+	new_fields_set = set(hf2[schema_key].keys())
 
 assert(old_fields_set == new_fields_set)
 
@@ -19,17 +24,17 @@ sorted_field_list = sorted(list(old_fields_set))
 with h5py.File(old_source, 'r') as hf1:
 	d1 = {}
 	for field in sorted_field_list:
-		for k in list(hf1['vaccine_doses'][field].keys()):
+		for k in list(hf1[schema_key][field].keys()):
 			key = field + '@' + k
-			value = hf1['vaccine_doses'][field][k][:]
+			value = hf1[schema_key][field][k][:]
 			d1[key] = value
 
 with h5py.File(new_source, 'r') as hf2:
 	d2 = {}
 	for field in sorted_field_list:
-		for k in list(hf2['vaccine_doses'][field].keys()):
+		for k in list(hf2[schema_key][field].keys()):
 			key = field + '@' + k
-			value = hf2['vaccine_doses'][field][k][:]
+			value = hf2[schema_key][field][k][:]
 			d2[key] = value
 
 
@@ -57,3 +62,4 @@ for key in keys:
 		if v1[i] != v2[i]:
 			print(key)
 			print(i, v1[i], v2[i])
+			break
