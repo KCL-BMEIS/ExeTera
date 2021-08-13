15432e3ba6817f7fc6bc2d69cf2f8e0930bce3a9
Liyuan Chen <liyuan.chen.1024@gmail.com>
2021-08-13 13:56:50 +0100

    fix array get

--- exetera/core/dataframe.py
+++ exetera/core/dataframe.py
@@ -476,7 +476,7 @@ class HDF5DataFrame(DataFrame):
                         chunk_data.append(field.data[start_row: start_row+chunk_row_size].tolist())
 
                 for i, row in enumerate(zip(*chunk_data)):
-                    if filter_array is None or filter_array[i + start_row] == True:
+                    if filter_array is None or filter_array.get(i + start_row, False) == True:
                         writer.writerow(row)  
 
                 if len(chunk_data[0]) < chunk_row_size:
