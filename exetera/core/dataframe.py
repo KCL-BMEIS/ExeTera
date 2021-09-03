54a8d41985efcacc538e223050fdbe968374cd76
Liyuan Chen <liyuan.chen.1024@gmail.com>
2021-09-03 13:56:27 +0100

    to_csv() api using csv.writer

--- exetera/core/dataframe.py
+++ exetera/core/dataframe.py
@@ -18,6 +18,7 @@ from exetera.core import fields as fld
 from exetera.core import operations as ops
 from exetera.core import validation as val
 import h5py
+import csv as csvlib
 
 
 class HDF5DataFrame(DataFrame):
@@ -458,7 +459,7 @@ class HDF5DataFrame(DataFrame):
         elif kind != 'stable':
             raise ValueError("Currently sort_values() only supports kind='stable'")
 
-        keys = val.validate_sort_and_groupby_keys(by, self._columns.keys())
+        keys = val.validate_selected_keys(by, self._columns.keys())
 
         readers = tuple(self._columns[k] for k in keys)
 
@@ -466,6 +467,54 @@ class HDF5DataFrame(DataFrame):
             readers, np.arange(len(readers[0].data), dtype=np.uint32))
 
         return self.apply_index(sorted_index, ddf)
+
+
+    def to_csv(self, filepath:str, row_filter:Union[np.ndarray, fld.Field]=None, column_filter:Union[str, List[str]]=None, chunk_row_size:int=1<<15):
+        """
+        Write object to a comma-separated values (csv) file.
+        :param filepath: File path.
+        :param row_filter: A boolean array / field. Only select rows when filter value is True
+        :param column_filter: A sequence of string names for the fields.
+        :chunk_row_size: Write rows for every chunk which has maximum chunk_row_size rows. The default is 1<<15.
+        """
+        column_names = self.keys()
+        column_fields = self.values()
+        field_name_to_use = list(self.keys())
+        if column_filter is not None:
+            field_name_to_use = val.validate_selected_keys(column_filter, self.keys())  
+
+        filter_array = None
+        if row_filter is not None:
+            filter_array, is_field = val.validate_boolean_row_filter('row_filter', row_filter)
+            if is_field and row_filter.name in field_name_to_use:
+                field_name_to_use.remove(row_filter.name)
+
+        fields_to_use = [self._columns[f] for f in field_name_to_use]
+
+        with open(filepath, 'w') as f:
+            writer = csvlib.writer(f, delimiter=',',lineterminator='\n')
+
+            # write header names
+            writer.writerow(field_name_to_use)
+
+            start_row = 0
+            while True:
+                chunk_data = []
+                for field in fields_to_use:
+                    if field.indexed:
+                        chunk_data.append(field.data[start_row: start_row+chunk_row_size])
+                    else:
+                        chunk_data.append(field.data[start_row: start_row+chunk_row_size].tolist())
+
+                for i, row in enumerate(zip(*chunk_data)):
+                    if filter_array is None or (i + start_row <len(filter_array) and filter_array[i + start_row] == True):
+                        writer.writerow(row)
+
+                if len(chunk_data[0]) < chunk_row_size:
+                    break
+                else:
+                    start_row += chunk_row_size
+
 
 
 def copy(field: fld.Field, dataframe: DataFrame, name: str):
