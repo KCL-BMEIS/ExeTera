e0cb129b03944be1c3bf4ae4160deee5f1533e0d
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2022-03-29 17:25:16 +0100

    Better versioning

--- exetera/_version.py
+++ exetera/_version.py
@@ -1,24 +1,2 @@
 
-#__version__ = '0.6.0b'
-
-import re
-from os import path
-
-try:
-    import importlib.metadata as imeta
-except ImportError:
-    import importlib_metadata as imeta
-
-try:
-    __version__ = imeta.version("exetera")
-except:
-    filename="pyproject.toml"
-    
-    if not path.exists(filename):
-        this_directory = path.abspath(path.dirname(__file__))
-        filename = path.abspath(path.join(this_directory, "..", filename))
-    
-    with open(filename) as o:
-        dat=o.read()
-        __version__ = re.search("version *= *\"(.*)\"", dat).group(1)
-
+__version__ = '0.6.0b0'
