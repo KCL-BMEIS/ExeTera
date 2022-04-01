69706be73016e11bf28ee3e6e26928575582b6e2
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2022-04-01 12:52:35 +0100

    [WIP] Moving to pyproject.toml Based Packaging (#280)
    * Adding option to disable Numba during tests to run code as pure Python to get coverage information
    
    * Add check for correctly raised warning
    
    * Update to unitest job name
    
    * Fix
    
    * Adding journal test back
    
    Signed-off-by: Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
    
    * Install problem fix
    
    * Install problem fix
    
    * Moving to toml based packaging
    
    * Better versioning
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Tweaking action
    
    * Tweaking action
    
    * Tweak

--- setup.py
+++ setup.py
@@ -1,36 +1,5 @@
-from setuptools import setup, find_packages
-from pkg_resources import parse_requirements
-#from Cython.Build import cythonize
 
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+from setuptools import setup
 
-# read the version and other strings from _version.py
-with open(path.join(this_directory, "exetera", "_version.py")) as o:
-    exec(o.read())
-
-# read install requirements from requirements.txt
-with open(path.join(this_directory, "requirements.txt")) as o:
-    requirements = [str(r) for r in parse_requirements(o.read())]
-
-#pyxfiles = ['ops.pyx']
-#pyx_full_path = [path.join(this_directory, 'exetera', '_libs', pyx) for pyx in pyxfiles]
-
-setup(
-    name='exetera',
-    version=__version__,
-    description='High-volume key-value store and analytics, based on hdf5',
-    long_description=long_description,
-    long_description_content_type = "text/markdown",
-    url='https://github.com/kcl-bmeis/ExeTera',
-    author='Ben Murray',
-    author_email='benjamin.murray@kcl.ac.uk',
-    license='http://www.apache.org/licenses/LICENSE-2.0',
-    packages=find_packages(),
-    scripts=['exetera/bin/exetera'],
-    #ext_modules = cythonize(pyx_full_path),
-    python_requires='>=3.7',
-    install_requires=requirements
-)
+if __name__ == "__main__":
+    setup()
