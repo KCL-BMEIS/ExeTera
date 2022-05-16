b7b8030c00b129379c08a29b66dae940de489159
Liyuan Chen <liyuan.chen.1024@gmail.com>
2022-05-16 14:17:28 +0100

    parameterized unittest for where

--- exetera/core/fields.py
+++ exetera/core/fields.py
@@ -55,12 +55,14 @@ def isin(field:Field, test_elements:Union[list, set, np.ndarray]):
 
 
 def where(cond, a, b):
-    if isinstance(cond, np.ndarray) and cond.dtype == 'bool':
+    if isinstance(cond, list) or (isinstance(cond, np.ndarray) and cond.dtype == 'bool'):
         cond = cond
     elif isinstance(cond, Field):
         if cond.indexed:
             raise NotImplementedError("Where does not support indexed string fields at present")
         cond = cond.data[:]
+    elif callable(cond):
+        raise NotImplementedError("fields.where doesn't support callable cond")
 
     if isinstance(a, Field):
         a = a.data[:]
@@ -177,15 +179,19 @@ class HDF5Field(Field):
 
         if callable(cond):
             cond = cond(self.data[:])
-        elif isinstance(cond, np.ndarray) and cond.dtype == 'bool':
+        elif isinstance(cond, list) or (isinstance(cond, np.ndarray) and cond.dtype == 'bool'):
             cond = cond
-        elif isinstance(cond, NumericMemField):
+        elif isinstance(cond, Field):
+            if cond.indexed:
+                raise NotImplementedError("Where does not support indexed string fields at present")
             cond = cond.data[:]
         else:
             raise TypeError("'cond' parameter needs to be either callable lambda function, or boolean ndarray, or NumericMemField")
 
         if isinstance(b, str):
             b = b.encode()
+        if isinstance(b, Field):
+            b = b.data[:]
 
         result = np.where(cond, self.data[:], b)
 
